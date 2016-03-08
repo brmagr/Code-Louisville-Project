904743ab9fe171efb5f305c967f317d746b42c3e
brmagr <brentgross@yahoo.com>
2016-03-07 23:32:19 -0500

    Worked on Map API and Angular Search Bar

---
+++ controller.js
@@ -0,0 +1,6 @@
+var searchApp = angular.module("searchApp", []);
+searchApp.controller("controller", function($scope) {
+$scope.menu = [
+    {"name": "Chicken"},
+    {"name": "Beef"},
+]
--- mapapi.js
+++ mapapi.js
@@ -1,39 +1,29 @@
-function myFunction() {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    document.getElementById("demo").innerHTML = "Width: " + w + "<br>Height: " + h;
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: {lat: 41.85, lng: -87.65}
+  });
+  directionsDisplay.setMap(map);
 
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('start').addEventListener('change', onChangeHandler);
+  document.getElementById('end').addEventListener('change', onChangeHandler);
 }
 
-function initMap() {
-  var end = {lat: 38.166551, lng: -85.602028};
-  var start   = {lat: ' ', lng: ' '};
-
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: end,
-      scrollwheel: false,
-      zoom: 7
-});
-
-var directionsDisplay = new google.maps.DirectionsRenderer({
-  map: map
-});
-
-var request = {
-  destination:  end,
-  origin: ' ',
-  travelMode: google.maps.TravelMode.DRIVING
-
-
-};
-
-var directionsService = new google.maps.directionsService();
-
-directionsService.route(request, function(response, status){
-
-if (status == google.maps.DirectionsStatus.OK) {
-
-directionsDisplay.setDirections(response);
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('start').value,
+    destination: document.getElementById('end').value,
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
     }
   });
 }
