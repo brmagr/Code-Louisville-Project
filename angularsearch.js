72a34739da3999b7f80a51a049f62ac1fe79966f
brmagr <brentgross@yahoo.com>
2016-03-11 22:48:36 -0500

    Worked on search bar/filter and added menu pages

--- angularsearch.js
+++ angularsearch.js
@@ -1,10 +1,45 @@
-app.directive('search', function () {
-    return function ($scope, element) {
-        element.bind("keyup", function (event) {
-          var val = element.val();
-          if(val.length > 2) {
-            $scope.search(val);
-          }
-        });
-    };
-});
+var searchApp = angular.module("SearchApp",[]);
+  searchApp.controller("seaCtrl",function($scope){
+    $scope.query = {}
+    $scope.queryBy = '$'
+    $scope.searches = [
+      {
+        "name" : "Appetizers"
+      },
+      {
+        "name" : "Side Trips"
+      },
+      {
+        "name" : "Sandwiches"
+      },
+      {
+        "name" : "Fish and Seafood"
+      },    {
+            "name" : "Mixed Platters"
+          },
+          {
+            "name" : "Dockside Delites"
+          },     {
+                "name" : "Galley Broiler"
+              },
+              {
+                "name" : "Shrimp Fest"
+              },     {
+                    "name" : "Chicken-Fest Platters"
+                  },
+                  {
+                    "name" : "Senior Citizens Platters"
+                  },     {
+                        "name" : "Wet Your Whistle"
+                      },
+                      {
+                        "name" : "Desserts"
+                      },     {
+                            "name" : "Little Mates Plates"
+                          },
+                          {
+                            "name" : "Lunch Menu"
+                          }
+    ];
+    $scope.orderProp="name";
+  });
